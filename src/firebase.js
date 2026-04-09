8df30681ae799fab37f8693566e5ae3a6c1906ec
Daibao <daibao@DaibaoMacBook-Air.local>
2026-04-09 16:26:18 +0800

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, signInAnonymously } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDi3E5jHjanuw-4huooIiJ5SqhWmdm2EP0",
+  authDomain: "b-battle-580b5.firebaseapp.com",
+  projectId: "b-battle-580b5",
+  storageBucket: "b-battle-580b5.firebasestorage.app",
+  messagingSenderId: "912827003016",
+  appId: "1:912827003016:web:febd90b0c5fc0484dcaf3d",
+  measurementId: "G-N45T9PP5V5"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export { signInAnonymously };
